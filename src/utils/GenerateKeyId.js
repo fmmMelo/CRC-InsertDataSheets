b8a982746433f148e48d6686dfce9ef737e82ddf
fmmMelo <felipe.moura@estudante.ifms.edu.br>
2020-05-10 03:36:40 -0300

    modifing folder structure

--- src/utils/GenerateKeyId.js
+++
@@ -1,9 +0,0 @@
-module.exports = function GenerateKey() {
-    // Math.random should be unique because of its seeding algorithm.
-    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-    // after the decimal.
-    
-    GetKeyId = Math.random().toString(36).substr(2, 9);
-
-    return GetKeyId;
-  };
